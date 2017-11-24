5f6fb85451417e01561181077512ed127222b5e2
Hannah Lily Postman <postman.hl@gmail.com>
2017-11-24 14:44:00 -0800

    Scaffold tab bar navigation to add collection view option and add custom cell class for that view

---
+++ FlicksTA/PosterCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  PosterCollectionViewCell.swift
+//  FlicksTA
+//
+//  Created by Aristotle on 2017-11-24.
+//  Copyright © 2017 HLPostman. All rights reserved.
+//
+
+import UIKit
+
+class PosterCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var posterImageView: UIImageView!
+    
+}
